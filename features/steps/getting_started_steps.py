8190203fde85e599ef8d564009f810c7b78e7c0b
Bas Dijkstra <bas@ontestautomation.com>
2023-10-05 10:38:55 +0200

    Initial commit: getting started

---
+++ features/steps/getting_started_steps.py
@@ -0,0 +1,21 @@
+from behave import *
+
+
+@given(u'we have properly installed behave')
+def step_impl(context):
+    pass
+
+
+@given(u'we have created step definitions for our scenario')
+def step_impl(context):
+    pass
+
+
+@when(u'we execute this scenario')
+def step_impl(context):
+    pass
+
+
+@then(u'we do not see unexpected behaviour')
+def step_impl(context):
+    pass
