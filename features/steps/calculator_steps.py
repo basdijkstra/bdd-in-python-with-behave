6024a38e3e32e220fbf05624fc17401adbbe2b08
Bas Dijkstra <bas@ontestautomation.com>
2026-07-08 16:15:25 +0200

    Add solution for calculator exercise

--- features/steps/calculator_steps.py
+++ features/steps/calculator_steps.py
@@ -3,22 +3,22 @@ from behave import given, when, then
 @given(u'the first number is {first}')
 def step_impl(context, first):
     # TODO: store the first number in the context
-    pass
+    context.first = int(first)
 
 
 @given(u'the second number is {second}')
 def step_impl(context, second):
     # TODO: store the second number in the context
-    pass
+    context.second = int(second)
 
 
 @when(u'the two numbers are added')
 def step_impl(context):
     # TODO: add the two numbers and store the result in the context
-    pass
+    context.result = context.first + context.second
 
 
 @then(u'the result should be {result}')
 def step_impl(context, result):
     # TODO: assert that the result stored in the context is equal to the value passed in the step
-    pass
+    assert context.result == int(result)
