de5968d35329f2c67ff8cd02e94ed5d9235986a7
Bas Dijkstra <bas@ontestautomation.com>
2026-07-08 16:26:09 +0200

    Add solution for loan application feature

--- features/steps/loan_application_steps.py
+++ features/steps/loan_application_steps.py
@@ -1,26 +1,33 @@
 from behave import *
 import requests
 
-# @given(u'{user} is an active ParaBank user')
-# def is_an_active_parabank_user(context, user):
-#     context.user = 12212
-#
-#
-# @when(u'they apply for a {amount} dollar loan')
-# def they_apply_for_a_loan(context, amount):
-#
-#     headers = {'Accept': 'application/json'}
-#     query_params = {'customerId': context.user, 'amount': amount, 'downPayment': 10, 'fromAccountId': 12345}
-#
-#     context.response = requests.post(
-#         f'http://localhost:8080/parabank/services/bank/requestLoan',
-#         headers=headers,
-#         params=query_params
-#     )
-#
-#
-# @then(u'the loan application is {result}')
-# def step_impl(context, result):
-#     response_json = context.response.json()
-#     print(response_json)
-#     assert response_json['approved'] == (result == 'approved')
+def before_scenario(context, scenario):
+    response = requests.post(
+        f'http://localhost:8080/parabank/services/bank/initializeDB'
+    )
+
+    assert response.status_code == 200
+
+@given(u'{user} is an active ParaBank customer')
+def is_an_active_parabank_user(context, user):
+    context.user = 12212
+
+
+@when(u'they apply for a {amount} dollar loan')
+def they_apply_for_a_loan(context, amount):
+
+    headers = {'Accept': 'application/json'}
+    query_params = {'customerId': context.user, 'amount': amount, 'downPayment': 10, 'fromAccountId': 12345}
+
+    context.response = requests.post(
+        f'http://localhost:8080/parabank/services/bank/requestLoan',
+        headers=headers,
+        params=query_params
+    )
+
+
+@then(u'the loan application is {result}')
+def step_impl(context, result):
+    response_json = context.response.json()
+    print(response_json)
+    assert response_json['approved'] == (result == 'approved')
